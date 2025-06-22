37724561df7b349f6511596d260e7ec55e91dc93
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-22 01:45:38 +0100

    Add host info to throughput table and benchmark script

---
+++ THRUPUT.md
@@ -0,0 +1,22 @@
+**Host**: Intel(R) Xeon(R) Platinum 8370C CPU @ 2.80GHz (x86_64, 5 CPUs)
+**Best configuration**: decode=1 prepare=1 dispatch=2
+
+| Shader | Cmds/s |
+|-------|-------:|
+| mov_tex | 817915.44 |
+| mul_const | 1235479.83 |
+| dp3_matrix | 953910.06 |
+| add | 1200614.18 |
+| matrix_ops | 30888687.78 |
+| tex_ops | 920261.87 |
+| terrain_ps | 62238237.64 |
+| motion_blur_vs | 8795750.01 |
+| river_water_ps | 1064780.31 |
+| water_reflection_ps | 93629688.78 |
+| water_trapezoid_ps | 85683921.78 |
+| max_min | 1213959.72 |
+| cnd | 1138842.37 |
+| nop | 0.00 |
+| ps13_ops | 122196788.49 |
+| tex_matrix | 1899035.12 |
+| **Overall** | 2249874.83 |
--- include/runtime_pipeline.h
+++ include/runtime_pipeline.h
@@ -3,6 +3,7 @@
 
 #include "minithread.h"
 #include "lf_queue.h"
+#include <time.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,16 +21,29 @@ typedef struct pipeline {
     lf_queue decode_q;
     lf_queue prepare_q;
     lf_queue dispatch_q;
-    int num_threads;
+    int decode_threads;
+    int prepare_threads;
+    int num_threads; /* dispatch threads */
     struct pipeline_stats *stats;
 } pipeline;
 
 double pipeline_commands_per_second(const pipeline *p);
 
 int pipeline_init(pipeline *p, int num_threads);
+int pipeline_init_stages(pipeline *p, int decode_threads, int prepare_threads,
+                         int dispatch_threads);
 int pipeline_start(pipeline *p);
 void pipeline_stop(pipeline *p);
 void pipeline_join(pipeline *p);
+
+static inline double ts_diff(const struct timespec *restrict s,
+                             const struct timespec *restrict e) {
+    return (e->tv_sec - s->tv_sec) + (e->tv_nsec - s->tv_nsec) / 1e9;
+}
+
+#define TS_DIFF(s, e)                                                         \
+    _Generic((s), struct timespec *: ts_diff, const struct timespec *: ts_diff)( \
+        (s), (e))
 
 #ifdef __cplusplus
 }
